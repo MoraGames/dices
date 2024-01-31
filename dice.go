719d90802879c4e2ca15c0c624f43f3e6f5bce37
MoraGames <andreamorandi2612@gmail.com>
2024-01-31 23:26:14 +0100

    (refactor) accept sides value of type 'any' interface.

--- dice.go
+++ dice.go
@@ -3,37 +3,39 @@ package dices
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 )
 
-const minSides int = 2
+const minimumSides int = 2
 
-type Dice struct {
-	rand *rand.Rand
+type (
+	Side any
+	Dice struct {
+		rand *rand.Rand
 
-	sidesNumber int
-	sidesValue  []string
-}
+		sidesNumber int
+		sidesValue  []Side
+	}
+)
 
 func NewDice(sidesNumber int) (*Dice, error) {
-	if sidesNumber < minSides {
-		return nil, fmt.Errorf("the number of faces (%d) must be greater than or equal to %d", sidesNumber, minSides)
+	if sidesNumber < minimumSides {
+		return nil, fmt.Errorf("the number of sides (%d) must be greater than or equal to %d", sidesNumber, minimumSides)
 	}
-	var sidesValue []string
+	var sidesValue []Side
 	for s := 1; s <= sidesNumber; s++ {
-		sidesValue = append(sidesValue, strconv.Itoa(s))
+		sidesValue = append(sidesValue, s)
 	}
 	return &Dice{rand.New(rand.NewSource(time.Now().UnixNano())), sidesNumber, sidesValue}, nil
 }
 
-func NewCustomDice(sidesValue ...string) (*Dice, error) {
-	if len(sidesValue) < minSides {
-		return nil, fmt.Errorf("the number of faces (%d) must be greater than or equal to %d", len(sidesValue), minSides)
+func NewCustomDice(sidesValue ...Side) (*Dice, error) {
+	if len(sidesValue) < minimumSides {
+		return nil, fmt.Errorf("the number of sides (%d) must be greater than or equal to %d", len(sidesValue), minimumSides)
 	}
 	return &Dice{rand.New(rand.NewSource(time.Now().UnixNano())), len(sidesValue), sidesValue}, nil
 }
 
-func (d *Dice) Throw() string {
+func (d *Dice) Throw() Side {
 	return d.sidesValue[d.rand.Intn(d.sidesNumber)]
 }
--- set.go
+++ set.go
@@ -2,7 +2,7 @@ package dices
 
 import "fmt"
 
-const minDices int = 1
+const minimumDices int = 1
 
 type Set struct {
 	dices []*Dice
@@ -10,13 +10,13 @@ type Set struct {
 
 func NewSet(dices ...*Dice) (*Set, error) {
 	if len(dices) == 0 {
-		return nil, fmt.Errorf("the number of dices (%d) must be greater than or equal to %d", len(dices), minDices)
+		return nil, fmt.Errorf("the number of dices (%d) must be greater than or equal to %d", len(dices), minimumDices)
 	}
 	return &Set{dices}, nil
 }
 
-func (s *Set) Throw() []string {
-	result := make([]string, 0)
+func (s *Set) Throw() []Side {
+	result := make([]Side, 0)
 	for i := 0; i < len(s.dices); i++ {
 		result = append(result, s.dices[i].Throw())
 	}
